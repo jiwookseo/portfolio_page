4ff39b3f1603727736536019f2c13f02593f2591
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-07-08 13:36:38 +0900

    Add AOS, Vue-SimpleMDE, SCSS S1P1212006-35

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import VueSimplemde from 'vue-simplemde'
+import 'simplemde/dist/simplemde.min.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -7,9 +9,19 @@ import store from './store'
 import "expose-loader?$!expose-loader?jQuery!jquery";
 window.$ = window.jQuery = require("jquery");
 
-Vue.config.productionTip = false
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueSimplemde);
 
 new Vue({
+  created() {
+    AOS.init({
+      once: false,
+    });
+  },
   router,
   store,
   render: h => h(App)
