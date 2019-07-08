7a55a6df952d772326a3838808b16da336fac1c3
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-07-08 14:54:41 +0900

    Add Header, HomePage, Footer S1P1212006-36

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+  <v-app id="app">
     <v-content>
-      <HelloWorld/>
+      <router-view />
+      <Footer />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Footer
   },
   data () {
     return {
@@ -36,3 +22,10 @@ export default {
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+@import './css/reset.css';
+@import './css/style.css';
+
+</style>
