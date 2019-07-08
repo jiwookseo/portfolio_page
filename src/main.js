a7ae327369c320e56d3a228a2baac2ff0ab37b6e
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-07-08 11:18:01 +0900

    Add Vuetify and JQuery

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import "expose-loader?$!expose-loader?jQuery!jquery";
+window.$ = window.jQuery = require("jquery");
 
 Vue.config.productionTip = false
 
