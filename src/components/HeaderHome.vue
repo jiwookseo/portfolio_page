43a32f88d75535e799cc8367e2e9aadbe8e4d652
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-07-10 17:41:53 +0900

    update view and components for translate
    Add className text to tags have innerText

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -1,150 +1,163 @@
 <template>
-    <header class="homeHeader">
-        <ImgBanner :imgSrc="imgSrc" />
-        <nav class="navbar">
-            <div class="nav-logo">{{websiteTitle}}</div>
-            <FavBtn class="nav-fav" />
-            <div v-if="!getUser" class="login-btn" @click.stop="dialog = true">Login</div>
-            <div v-if="getUser" @click="signOut" class="login-btn" >Logout</div>
-            <ul class="nav-menus">
-                <li v-for="menu in menus" :key="menu.name" @click="scrollTo(menu.target)">{{ menu.name }}</li>
-            </ul>
-        </nav>
-        <transition name="slide-fade">
-            <nav class="sidebar" v-if="showSidebar">
-                <div class="sb-nav-logo">{{websiteTitle}}</div>
-                <ul>
-                    <li v-for="menu in menus" class="sb-nav-menu" :key="menu.name" @click="sb_scrollTo(menu.target)">{{ menu.name }}</li>
-                    <li v-if="!getUser" class="sb-nav-menu" @click.stop="sb_login">Login</li>
-                    <li v-if="getUser" class="sb-nav-menu" @click="signOut">Logout</li>
-                </ul>
-            </nav>
-        </transition>
-        <div class="menu-btn" @click="showSidebar = !showSidebar">
-            <span v-if="showSidebar"><i class="material-icons">clear</i></span>
-            <span v-if="!showSidebar"><i class="material-icons">dehaze</i></span>
-        </div>
-        <FavBtn class="sb-fav"/>
-        <v-dialog v-model="dialog" class="login-dialog" width="400">
-            <LoginDialog @child="parents"/>
-        </v-dialog>
-    </header>
+  <header class="homeHeader">
+    <ImgBanner :imgSrc="imgSrc" />
+    <nav class="navbar">
+      <div class="nav-logo text">{{websiteTitle}}</div>
+      <FavBtn class="nav-fav" />
+      <div v-if="!getUser" class="login-btn text" @click.stop="dialog = true">Login</div>
+      <div v-if="getUser" @click="signOut" class="login-btn text">Logout</div>
+      <ul class="nav-menus">
+        <li
+          v-for="menu in menus"
+          class="text"
+          :key="menu.name"
+          @click="scrollTo(menu.target)"
+        >{{ menu.name }}</li>
+      </ul>
+    </nav>
+    <transition name="slide-fade">
+      <nav class="sidebar" v-if="showSidebar">
+        <div class="sb-nav-logo">{{websiteTitle}}</div>
+        <ul>
+          <li
+            v-for="menu in menus"
+            class="sb-nav-menu"
+            :key="menu.name"
+            @click="sb_scrollTo(menu.target)"
+          >{{ menu.name }}</li>
+          <li v-if="!getUser" class="sb-nav-menu text" @click.stop="sb_login">Login</li>
+          <li v-if="getUser" class="sb-nav-menu text" @click="signOut">Logout</li>
+        </ul>
+      </nav>
+    </transition>
+    <div class="menu-btn" @click="showSidebar = !showSidebar">
+      <span v-if="showSidebar">
+        <i class="material-icons">clear</i>
+      </span>
+      <span v-if="!showSidebar">
+        <i class="material-icons">dehaze</i>
+      </span>
+    </div>
+    <FavBtn class="sb-fav" />
+    <v-dialog v-model="dialog" class="login-dialog" width="400">
+      <LoginDialog @child="parents" />
+    </v-dialog>
+  </header>
 </template>
 
 <script>
-import ImgBanner from './ImgBanner';
-import FavBtn from './FavBtn';
-import LoginDialog from './LoginDialog';
-import { mapActions, mapGetters } from 'vuex'
+import ImgBanner from "./ImgBanner";
+import FavBtn from "./FavBtn";
+import LoginDialog from "./LoginDialog";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-    name: "HeaderHome",
-    components: {
-        ImgBanner,
-        FavBtn,
-        LoginDialog
+  name: "HeaderHome",
+  components: {
+    ImgBanner,
+    FavBtn,
+    LoginDialog
+  },
+  props: {
+    websiteTitle: { type: String }
+  },
+  computed: {
+    ...mapGetters(["getUser"])
+  },
+  data() {
+    return {
+      imgSrc: "https://source.unsplash.com/random/1600x900/",
+      dialog: false,
+      showSidebar: false,
+      menus: [
+        { name: "About Me", target: "#aboutme" },
+        { name: "Portfolio", target: "#portfolio" },
+        { name: "Post", target: "#post" },
+        { name: "Project", target: "#project" }
+      ]
+    };
+  },
+  methods: {
+    ...mapActions(["logout"]),
+    signOut() {
+      alert("로그아웃 되었습니다!");
+      this.logout();
+      this.$router.replace("/");
     },
-    props: {
-        websiteTitle: {type: String}
+    scrollTo(target) {
+      this.$parent.scrollTo(target);
     },
-    computed: {
-      ...mapGetters(['getUser'])
+    sb_scrollTo(target) {
+      this.$parent.scrollTo(target);
+      this.showSidebar = false;
     },
-    data() {
-        return {
-            imgSrc: "https://source.unsplash.com/random/1600x900/",
-            dialog: false,
-            showSidebar: false,
-            menus: [
-                {name: 'About Me', target: '#aboutme'},
-                {name: 'Portfolio', target: '#portfolio'},
-                {name: 'Post', target: '#post'},
-                {name: 'Project', target: '#project'}
-            ]
-        }
+    sb_login() {
+      this.showSidebar = false;
+      this.dialog = true;
     },
-    methods: {
-        ...mapActions(['logout']),
-        signOut () {
-          alert("로그아웃 되었습니다!")
-          this.logout()
-          this.$router.replace('/')
-        },
-        scrollTo(target) {
-            this.$parent.scrollTo(target);
-        },
-        sb_scrollTo(target) {
-            this.$parent.scrollTo(target);
-            this.showSidebar = false;
-        },
-        sb_login() {
-            this.showSidebar = false;
-            this.dialog = true;
-        },
-        parents(dialog) {
-          this.dialog = dialog
-        }
+    parents(dialog) {
+      this.dialog = dialog;
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../css/mixin.scss';
-@import '../css/navbar.scss';
-@import '../css/style.scss';
+@import "../css/mixin.scss";
+@import "../css/navbar.scss";
+@import "../css/style.scss";
 .homeHeader {
-    width: 100%;
-    height: calc(100vh + 50px);
-    position: relative;
-    @include mobile {
-        height: 100vh;
-    }
+  width: 100%;
+  height: calc(100vh + 50px);
+  position: relative;
+  @include mobile {
+    height: 100vh;
+  }
 }
 .navbar {
-    position: absolute;
-    bottom: 0;
-    left: 0;
+  position: absolute;
+  bottom: 0;
+  left: 0;
 }
 .nav-logo {
+  float: left;
+  padding: 0 20px;
+  cursor: pointer;
+  &:hover {
+    background: $nav-highlight;
+  }
+}
+.nav-menus {
+  float: right;
+  li {
     float: left;
     padding: 0 20px;
     cursor: pointer;
-        &:hover {
-            background: $nav-highlight;
-        }
-}
-.nav-menus {
-    float: right;
-    li {
-        float: left;
-        padding: 0 20px;
-        cursor: pointer;
-        &:hover {
-            background: $nav-highlight;
-        }
+    &:hover {
+      background: $nav-highlight;
     }
+  }
 }
 .login-btn {
-    float: right;
-    padding: 0 20px;
-    cursor: pointer;
-    &:hover {
-        background: $nav-highlight;
-    }
+  float: right;
+  padding: 0 20px;
+  cursor: pointer;
+  &:hover {
+    background: $nav-highlight;
+  }
 }
 
 .nav-fav {
-    float: right;
+  float: right;
 }
 .sb-fav {
-    background: rgba(240, 248, 255, 0.5);
-    display: none;
-    position: fixed;
-    top: 0;
-    right: 0;
-    @include mobile {
-        display: initial;
-    }
+  background: rgba(240, 248, 255, 0.5);
+  display: none;
+  position: fixed;
+  top: 0;
+  right: 0;
+  @include mobile {
+    display: initial;
+  }
 }
-
 </style>
--- src/components/HeaderSub.vue
+++ src/components/HeaderSub.vue
@@ -1,94 +1,110 @@
 <template>
-    <header>
-        <nav class="navbar">
-            <div class="nav-item"><router-link to="/">HOME</router-link></div>
-            <div class="btn-right">
-                <router-link to="/portfolio"><div class="nav-item">Portfolio</div></router-link>
-                <router-link to="/post"><div class="nav-item">Post</div></router-link>
-                <div class="nav-item login-btn" @click.stop="dialog = true">Login</div>
-            </div>
-        </nav>
-        <transition name="slide-fade">
-            <nav class="sidebar" v-if="showSidebar">
-                <router-link to="/"><div class="sb-nav-logo">HOME</div></router-link>
-                <ul>
-                    <router-link to="/portfolio"><li class="sb-nav-menu">Portfolio</li></router-link>
-                    <router-link to="/post"><li class="sb-nav-menu">Post</li></router-link>
-                    <li class="sb-nav-menu" @click.stop="sb_login">Login</li>
-                </ul>
-            </nav>
-        </transition>
-        <div class="menu-btn" @click="showSidebar = !showSidebar">
-            <span v-if="showSidebar"><i class="material-icons">clear</i></span>
-            <span v-if="!showSidebar"><i class="material-icons">dehaze</i></span>
-        </div>
-        <FavBtn class="nav-fav" />
-        <v-dialog v-model="dialog" class="login-dialog" width="400">
-            <LoginDialog />
-        </v-dialog>
-    </header>
+  <header>
+    <nav class="navbar">
+      <div class="nav-item text">
+        <router-link to="/">HOME</router-link>
+      </div>
+      <div class="btn-right">
+        <router-link to="/portfolio">
+          <div class="nav-item text">Portfolio</div>
+        </router-link>
+        <router-link to="/post">
+          <div class="nav-item text">Post</div>
+        </router-link>
+        <div class="nav-item login-btn text" @click.stop="dialog = true">Login</div>
+      </div>
+    </nav>
+    <transition name="slide-fade">
+      <nav class="sidebar" v-if="showSidebar">
+        <router-link to="/">
+          <div class="sb-nav-logo text">HOME</div>
+        </router-link>
+        <ul>
+          <router-link to="/portfolio">
+            <li class="sb-nav-menu text">Portfolio</li>
+          </router-link>
+          <router-link to="/post">
+            <li class="sb-nav-menu text">Post</li>
+          </router-link>
+          <li class="sb-nav-menu text" @click.stop="sb_login">Login</li>
+        </ul>
+      </nav>
+    </transition>
+    <div class="menu-btn" @click="showSidebar = !showSidebar">
+      <span v-if="showSidebar">
+        <i class="material-icons">clear</i>
+      </span>
+      <span v-if="!showSidebar">
+        <i class="material-icons">dehaze</i>
+      </span>
+    </div>
+    <FavBtn class="nav-fav" />
+    <v-dialog v-model="dialog" class="login-dialog" width="400">
+      <LoginDialog />
+    </v-dialog>
+  </header>
 </template>
 
 <script>
-import FavBtn from './FavBtn';
-import LoginDialog from './LoginDialog';
+import FavBtn from "./FavBtn";
+import LoginDialog from "./LoginDialog";
 
 export default {
-    name: "HeaderSub",
-    components: {
-        FavBtn,
-        LoginDialog
-    },
-    props: {
-    },
-    data() {
-        return {
-            dialog: false,
-            showSidebar: false,
-        }
-    },
-    methods: {
-        sb_login() {
-            this.showSidebar = false;
-            this.dialog = true;
-        }
+  name: "HeaderSub",
+  components: {
+    FavBtn,
+    LoginDialog
+  },
+  props: {},
+  data() {
+    return {
+      dialog: false,
+      showSidebar: false
+    };
+  },
+  methods: {
+    sb_login() {
+      this.showSidebar = false;
+      this.dialog = true;
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../css/mixin.scss';
-@import '../css/navbar.scss';
+@import "../css/mixin.scss";
+@import "../css/navbar.scss";
 header {
-    position: relative;
+  position: relative;
 }
-a, a:hover {
-    color: initial;
+a,
+a:hover {
+  color: initial;
 }
 .navbar {
-    position: fixed;
-    top: 0; left: 0;
-    .btn-right {
-        float: right;
-        margin-right: 50px;
-    }
+  position: fixed;
+  top: 0;
+  left: 0;
+  .btn-right {
+    float: right;
+    margin-right: 50px;
+  }
 }
 .nav-item {
-    float: left;
-    padding: 0 20px;
-    text-decoration: none;
-    cursor: pointer;
-    &:hover {
-        background: #BBBBBB;
-    }
+  float: left;
+  padding: 0 20px;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background: #bbbbbb;
+  }
 }
 .nav-fav {
-    position: fixed;
-    top: 0; right: 0;
-    @include mobile {
-        background: rgba(255, 255, 255, 0.3);
-    }
+  position: fixed;
+  top: 0;
+  right: 0;
+  @include mobile {
+    background: rgba(255, 255, 255, 0.3);
+  }
 }
-
-
 </style>
