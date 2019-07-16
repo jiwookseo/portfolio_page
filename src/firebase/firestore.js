155fbe7e040c4279d6bad1e51dc6d57d4fef5403
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-07-16 16:38:44 +0900

    Update firestore function

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,15 +1,18 @@
-import {firebaseApp} from './firebase';
-import Firebase from 'firebase/app';
+import { firebaseApp } from "./firebase";
+import Firebase from "firebase/app";
 
-const firestore = Firebase.firestore()
+const firestore = Firebase.firestore();
 
-const POSTS = 'posts'
-const PORTFOLIOS = 'portfolios'
+const POSTS = "posts";
+const PORTFOLIOS = "portfolios";
 
 export default {
   async getPosts() {
     let posts = [];
-    let snapshot = await firestore.collection(POSTS).orderBy('created_at', 'desc').get();
+    let snapshot = await firestore
+      .collection(POSTS)
+      .orderBy("created_at", "desc")
+      .get();
     snapshot.docs.forEach(doc => {
       posts.push({
         id: doc.id,
@@ -18,27 +21,36 @@ export default {
         created_at: doc.data().created_at
       });
     });
-    return posts
+    return posts;
   },
-  postPost(title, content) {
-    return firestore.collection(POSTS).add({
+  async postPost(title, content) {
+    await firestore.collection(POSTS).add({
       title,
       content,
       created_at: Firebase.firestore.FieldValue.serverTimestamp()
-    })
-  },
-  deletePost(docID) {
-    firestore.collection(POSTS).doc(docID).delete();
-  },
-  updatePost(docID, title, content) {
-    firestore.collection(POSTS).doc(docID).update({
-      title: title,
-      content: content,
     });
+  },
+  async deletePost(docID) {
+    await firestore
+      .collection(POSTS)
+      .doc(docID)
+      .delete();
+  },
+  async updatePost(docID, title, content) {
+    await firestore
+      .collection(POSTS)
+      .doc(docID)
+      .update({
+        title: title,
+        content: content
+      });
   },
   async getPortfolios() {
     let portfolios = [];
-    let snapshot = await firestore.collection(PORTFOLIOS).orderBy('created_at', 'desc').get();
+    let snapshot = await firestore
+      .collection(PORTFOLIOS)
+      .orderBy("created_at", "desc")
+      .get();
     snapshot.docs.forEach(doc => {
       portfolios.push({
         id: doc.id,
@@ -48,49 +60,53 @@ export default {
         created_at: doc.data().created_at
       });
     });
-    return portfolios
+    return portfolios;
   },
-  postPortfolio(title, content, img) {
-    return firestore.collection(PORTFOLIOS).add({
+  async postPortfolio(title, content, img) {
+    await firestore.collection(PORTFOLIOS).add({
       title,
       content,
       img,
       created_at: Firebase.firestore.FieldValue.serverTimestamp()
-    })
-  },
-  deletePortfolio(docID) {
-    firestore.collection(PORTFOLIOS).doc(docID).delete();
-  },
-  updatePortfolio(docID, title, content, img) {
-    firestore.collection(PORTFOLIOS).doc(docID).update({
-      title: title,
-      content: content,
-      img: img
     });
   },
+  async deletePortfolio(docID) {
+    await firestore
+      .collection(PORTFOLIOS)
+      .doc(docID)
+      .delete();
+  },
+  async updatePortfolio(docID, title, content, img) {
+    await firestore
+      .collection(PORTFOLIOS)
+      .doc(docID)
+      .update({
+        title: title,
+        content: content,
+        img: img
+      });
+  },
   logView(user, id, path, time) {
-    db.ref('LOG/' + user).set({
+    db.ref("LOG/" + user).set({
       userId: id,
       path: path,
       time: time
-    })
+    });
   },
   addLog(path, username, time) {
     let db = firebaseApp.database();
-    let log = db.ref('LOG');
-    log.push(
-      {
-        'path': path,
-        'username': username,
-        'time': time
-      }
-    );
+    let log = db.ref("LOG");
+    log.push({
+      path: path,
+      username: username,
+      time: time
+    });
   },
   async getLog() {
-    let data = []
+    let data = [];
 
     let db = await firebaseApp.database();
-    let ref = db.ref('LOG').orderByChild('time');
+    let ref = db.ref("LOG").orderByChild("time");
     ref.once("value", function(snapshot) {
       snapshot.forEach(function(childSnapshot) {
         data.push(childSnapshot.val());
@@ -98,4 +114,4 @@ export default {
     });
     return data;
   }
-}
+};
