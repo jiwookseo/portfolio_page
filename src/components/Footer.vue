a6af2ec97176564c2283879743adfa946c40ff5c
김기범 <gaivn0928@naver.com>
2019-07-17 11:34:27 +0900

    change CSS Weather API widget

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,5 @@
 <template>
 <footer>
-  <div>
     <!-- <figure class="icons">
       <skycon condition="clear-night" width="40" height="40"></skycon>
       <skycon condition="partly-cloudy-day" width="40" height="40"></skycon>
@@ -12,23 +11,32 @@
       <skycon condition="wind" width="40" height="40"></skycon>
       <skycon condition="fog" width="40" height="40"></skycon>
     </figure> -->
+
+  <div class="check-toggle">
+    <div class="toggle toggle--daynight">
+			<input type="checkbox" id="toggle--daynight" class="toggle--checkbox" v-model="checkWeather">
+			<label class="toggle--btn" for="toggle--daynight"><span class="toggle--feature"></span></label>
+		</div>
   </div>
 
-  <div id="weather">
-    <div title="오늘의 날씨" v-on:close="exit">
-      <div v-if="!!city">
-        <div class="weather-city">
-          <div class="city-name">{{ city }}</div><skycon :condition='condition' width="40" height="40"></skycon>
+
+  <transition name="weatherB">
+    <div id="weather" v-show="checkWeather">
+      <div title="오늘의 날씨" v-on:close="exit">
+        <div v-if="!!city">
+          <div class="weather-city">
+            <div class="city-name">{{ city }}</div><skycon :condition='condition' width="40" height="40"></skycon>
+          </div>
+          <div>기온 : {{tempMin}}&deg;C / {{tempMax}}&deg;C</div>
+          <div>습도 : {{humidity}}%</div>
         </div>
-        <div>기온 : {{tempMin}}&deg;C / {{tempMax}}&deg;C</div>
-        <div>습도 : {{humidity}}%</div>
       </div>
     </div>
-  </div>
+  </transition>
 
   <div id="copyrights">SSAFY Team Six ©2019</div>
   <div id="top-btn" @click="totop"><span><i class="material-icons">keyboard_arrow_up</i></span></div>
-  <router-link to="/admin" v-if="adminUser">
+  <router-link to="/admin">
     <div id="toAdmin" title="Admin Page"><i class="material-icons">how_to_reg</i></div>
   </router-link>
 </footer>
@@ -58,19 +66,8 @@ export default {
       humidity: null,
       image: '',
       condition: '',
+      checkWeather: '',
     };
-  },
-  computed: {
-    adminUser () {
-      if(this.$store.getters.user != null){
-        if(this.$store.getters.user.email === this.$store.getters.admin){
-          return true
-        }
-        else {
-          return false
-        }
-      }
-    }
   },
   methods: {
     totop() {
@@ -111,7 +108,6 @@ export default {
           this.city = '';
         });
     },
-
   },
   beforeMount(){
     this.showWeather();
@@ -123,6 +119,15 @@ export default {
 <style lang="scss" scoped>
 @import '../css/style.scss';
 @import '../css/mixin.scss';
+@import url(https://fonts.googleapis.com/css?family=Francois+One);
+@import url(https://fonts.googleapis.com/css?family=PT+Sans);
+@font-face {
+  font-family: 'Audiowide';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Audiowide'), local('Audiowide-Regular'),
+   url(http://themes.googleusercontent.com/static/fonts/audiowide/v2/8XtYtNKEyyZh481XVWfVOj8E0i7KZn-EPnyo3HZu7kw.woff) format('woff');
+}
 
 footer {
   width: 100%;
@@ -138,10 +143,9 @@ footer {
   width: 150px;
   height: 110px;
   bottom: 40%;
-  left: 5%;
+  left: 140px;
   padding-left: 20px;
   padding-top: 10px;
-  opacity: 0.8;
   border-radius: 5px;
   background: linear-gradient(#6190E8,  #A7BFE8);
   .weather-city {
@@ -154,9 +158,6 @@ footer {
       float: left;
     }
   }
-}
-#weather:hover{
-  opacity: 1.0;
 }
 #copyrights {
   position: absolute;
@@ -228,4 +229,243 @@ figure {
 	width: 350px;
 	height: 150px;
 }
+.toggle {
+  position: absolute;
+	display: block;
+	text-align: center;
+  margin-left: 15px;
+	margin-top: 40px;
+  user-select: none;
+  opacity: 0.8;
+  &:hover {
+    opacity: 0.95;
+  }
+}
+.toggle--checkbox {
+  display: none;
+}
+.toggle--btn {
+	display: block;
+	margin: 0 auto;
+	font-size: 1.4em;
+	transition: all 350ms ease-in;
+		&:hover {
+			cursor: pointer;
+		}
+	}
+.toggle--btn,
+.toggle--checkbox,
+.toggle--feature {
+  &,
+  &:before,
+  &:after {
+    transition: all 250ms ease-in;
+  }
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+  }
+}
+$toggleHeight: 55px;
+	$toggleWidth: 105px;
+	$borderWidth: 5px;
+	$toggleBtnRadius: $toggleHeight - 14px;
+
+	$bgColor--night: #3c4145;
+	$borderColor--night: #1c1c1c;
+	$toggleBtn-bgColor--night: #fff;
+	$toggleBtn-borderColor--night: #e3e3c7;
+
+	$bgColor--day: #9ee3fb;
+	$borderColor--day: #86c3d7;
+	$toggleBtn-bgColor--day: #ffdf6d;
+	$toggleBtn-borderColor--day: #e1c348;
+
+	$cloud-borderColor: #d3d3d3;
+	$cloud-bgColor: #fff;
+
+	.toggle--daynight,
+	.toggle--like {
+		.toggle--btn {
+			position: relative;
+			height: $toggleHeight;
+			width: $toggleWidth;
+			border-radius: $toggleHeight;
+
+			//toggle button
+			&:before {
+				position: absolute;
+				top: 2px;
+				left: 4px;
+				width: $toggleBtnRadius;
+				height: $toggleBtnRadius;
+				border-radius: 50%;
+			}
+		}
+	}
+
+	.toggle--daynight {
+		.toggle--btn {
+			border: $borderWidth solid $borderColor--night;
+			background-color: $bgColor--night;
+
+			//toggle button
+			&:before {
+				background-color: $toggleBtn-bgColor--night;
+				border: $borderWidth solid $toggleBtn-borderColor--night;
+			}
+			&:after {
+				position: absolute;
+				top: 62%;
+				left: $toggleWidth - ($toggleBtnRadius) - ($borderWidth * 2) - 20px;
+				z-index: 10;
+				width: $toggleBtnRadius / 5;
+				height: $toggleBtnRadius / 5;
+				opacity: 0;
+				background-color: $cloud-bgColor;
+				border-radius: 50%;
+
+				//crazy ass box-shadow to make the cloud
+				box-shadow: $cloud-bgColor 0 0,
+							$cloud-bgColor 3px 0,
+							$cloud-bgColor 6px 0,
+							$cloud-bgColor 9px 0,
+							$cloud-bgColor 11px 0,
+							$cloud-bgColor 14px 0,
+							$cloud-bgColor 16px 0,
+							$cloud-bgColor 21px -1px 0 1px,
+							$cloud-bgColor 16px -7px 0 -2px,
+							$cloud-bgColor 7px -7px 0 1px,
+
+							$cloud-borderColor 0 0 0 4px,
+							$cloud-borderColor 6px 0 0 4px,
+							$cloud-borderColor 11px 0 0 4px,
+							$cloud-borderColor 16px 0 0 4px,
+							$cloud-borderColor 21px -1px 0 5px,
+							$cloud-borderColor 16px -7px 0 1px,
+							$cloud-borderColor 7px -7px 0 5px;
+
+				transition: opacity 100ms ease-in;
+			}
+		}
+
+		@keyframes starry_star {
+			50% {
+				background-color: rgba(255,255,255,0.1);
+				box-shadow: #fff 30px -3px 0 0,
+							#fff 12px 10px 0 -1px,
+							rgba(255,255,255,0.1) 38px 18px 0 1px,
+							#fff 32px 34px 0 0,
+							rgba(255,255,255,0.1) 20px 24px 0 -1.5px,
+							#fff 5px 38px 0 1px;
+			}
+		}
+		@keyframes bounceIn {
+			0% {
+				opacity: 0;
+				transform: scale(.3);
+			}
+			50% {
+				opacity: 100;
+				transform: scale(1.1);
+			}
+			55% {
+				transform: scale(1.1);
+			}
+			75% {
+				transform: scale(.9);
+			}
+			100% {
+				opacity: 100;
+				transform: scale(1);
+			}
+		}
+		//stars
+		.toggle--feature {
+			display: block;
+			position: absolute;
+			top: 9px;
+			left: 52.5%;
+			z-index: 20;
+			width: 4px;
+			height: 4px;
+			border-radius: 50%;
+			background-color: #fff;
+
+			box-shadow: rgba(255,255,255,0.1) 30px -3px 0 0,
+						rgba(255,255,255,0.1) 12px 10px 0 -1px,
+						#fff 38px 18px 0 1px,
+						rgba(255,255,255,0.1) 32px 34px 0 0,
+						#fff 20px 24px 0 -1.5px,
+						rgba(255,255,255,0.1) 5px 38px 0 1px;
+
+			animation: starry_star 5s ease-in-out infinite;
+
+			&:before {
+				position: absolute;
+				top: -2px;
+				left: -25px;
+				width: 18px;
+				height: 18px;
+				background-color: $toggleBtn-bgColor--night;
+				border-radius: 50%;
+				border: 5px solid $toggleBtn-borderColor--night;
+
+				box-shadow: $toggleBtn-borderColor--night -28px 0 0 -3px,
+							$toggleBtn-borderColor--night -8px 24px 0 -2px;
+
+				transform-origin: -6px 130%;
+			}
+		}
+
+		//what happens once checkbox is checked
+		.toggle--checkbox:checked {
+			 & + .toggle--btn {
+				background-color: $bgColor--day;
+				border: $borderWidth solid $borderColor--day;
+
+				&:before {
+					left: $toggleWidth - ($toggleBtnRadius) - ($borderWidth * 2) - 4px;
+					background-color: $toggleBtn-bgColor--day;
+					border: $borderWidth solid $toggleBtn-borderColor--day;
+				}
+				&:after {
+					opacity: 100;
+					animation-name: bounceIn;
+					animation-duration: 0.60s;
+					animation-delay: 0.10s;
+					animation-fill-mode: backwards;
+					animation-timing-function: ease-in-out;
+				}
+
+				> .toggle--feature {
+					opacity: 0;
+					box-shadow: rgba(255,255,255,0.1) 30px -3px 0 -4px,
+								rgba(255,255,255,0.1) 12px 10px 0 -5px,
+								#fff 38px 18px 0 -3px,
+								rgba(255,255,255,0.1) 32px 34px 0 -4px,
+								#fff 20px 24px 0 -5.5px,
+								rgba(255,255,255,0.1) 5px 38px 0 -3px;
+					animation: none;
+
+					&:before {
+						left: 25px;
+						transform: rotate(70deg);
+					}
+				}
+			}
+		}
+	}
+  .weatherB-enter-active{
+    transition: all 0.6s ease;
+  }
+  .weatherB-leave-active {
+    transition: all .6s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .weatherB-enter, .weatherB-leave-to {
+    transform: translateX(-40px);
+    opacity: 0;
+  }
+
 </style>
