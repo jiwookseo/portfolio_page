cfa8e46dbb5a089a5f9af85d67d9bae65d00e2fa
Jiwon Yoon <jiwonjulietyoon@gmail.com>
2019-07-23 04:53:24 +0000

    Revert "Start project (initial files cloned from Webmobile Project 2)"
    This reverts commit d32515a72f7689484464eb2b998998b1467d7d4a

--- src/firebase/firebase.js
+++
@@ -1,5 +0,0 @@
-import * as firebase from 'firebase'
-import firebaseConfig from './config'
-
-// Initialize Firebase
-export const firebaseApp = firebase.initializeApp(firebaseConfig.firebase);
--- src/firebase/firestorage.js
+++
@@ -1,4 +0,0 @@
-import { firebaseApp } from './firebase'
-import 'firebase/storage'
-
-export const firestorage = firebaseApp.storage()
