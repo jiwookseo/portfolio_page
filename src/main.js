465cd8526f56a02e5b7f12475d1a4cb44648a515
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-07-25 15:07:52 +0900

    [FIX] Fix some error

--- src/main.js
+++ src/main.js
@@ -1,40 +1,39 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import VueSimplemde from 'vue-simplemde'
-import 'simplemde/dist/simplemde.min.css'
-import App from './App.vue'
-import router from './router'
-import store from '@/vuex/store'
+import Vue from "vue";
+import "./plugins/vuetify";
+import VueSimplemde from "vue-simplemde";
+import "simplemde/dist/simplemde.min.css";
+import App from "./App.vue";
+import router from "./router";
+import store from "@/vuex/store";
 
 import "expose-loader?$!expose-loader?jQuery!jquery";
 window.$ = window.jQuery = require("jquery");
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import * as firebase from 'firebase'
-import VueSweetalert2 from 'vue-sweetalert2'
-import 'sweetalert2/dist/sweetalert2.min.css';
-import './registerServiceWorker'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import * as firebase from "firebase";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueSimplemde);
 Vue.use(VueSweetalert2);
-const { VUE_APP_ACCESS_TOKKEN } = process.env;
 
 new Vue({
   created() {
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.$store.dispatch('autoSignIn', user)
-        console.log(user.email)
+        this.$store.dispatch("autoSignIn", user);
+        console.log(user.email);
       }
-    })
+    });
     AOS.init({
-      once: true,
+      once: true
     });
   },
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
