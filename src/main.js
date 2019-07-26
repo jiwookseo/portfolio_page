868e8e25a40e3cf21828fb42261da0a74ad719a4
김기범 <gaivn0928@naver.com>
2019-07-26 15:48:49 +0900

    [MOD] modify push alarm source
    modify push alarm source by using token
    
    Jira : S1P1213006-3

--- src/main.js
+++ src/main.js
@@ -16,6 +16,41 @@ import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 import "./registerServiceWorker";
 
+var config = {
+  apiKey: "AIzaSyCPsjIgD6dIjp1o9Xd56bsBW64VMwdX6x8",
+  authDomain: "ssafy-teamsix.firebaseapp.com",
+  databaseURL: "https://ssafy-teamsix.firebaseio.com",
+  projectId: "ssafy-teamsix",
+  storageBucket: "ssafy-teamsix.appspot.com",
+  messagingSenderId: "207653910315",
+  appId: "1:207653910315:web:33636c136bf2abae"
+}; // 4. Get Firebase Configuration
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey(
+  "BBBpw-_z752QFHrMQElXJSrxpEB8_vKtiKUgD6sQvuuIbLSS5odGEeh--BzMYSqxYw3ikgePoUgQKPFJK1ed9D0"
+); // 1. Generate a new key pair
+
+// Request Permission of Notifications
+messaging
+  .requestPermission()
+  .then(() => {
+    console.log("Notification permission granted.");
+
+    // Get Token
+    messaging.getToken().then(token => {
+      console.log(token);
+    });
+  })
+  .catch(err => {
+    console.log("Unable to get permission to notify.", err);
+  });
+
 Vue.config.productionTip = false;
 
 Vue.use(VueSimplemde);
