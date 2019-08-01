76b5b5006532c84cfc17b74a1e1d25322b0b289e
김근태 <gtkim4617@naver.com>
2019-08-01 16:21:53 +0900

    [MOD]Encoding Type Change
    Korean available
    
    JIRA : S1P1213006-3

--- src/firebase/firebaseMessage.js
+++ src/firebase/firebaseMessage.js
@@ -22,19 +22,18 @@ export default {
         });
     });
   },
-  pushMessage(userToken) {
-    console.log(userToken);
+  pushMessage(userToken, title, body) {
     axios
       .post(
         "https://fcm.googleapis.com/fcm/send", {
           to: userToken,
           notification: {
-            title: "test",
-            body: "test"
+            title: title,
+            body: body
           }
         }, {
           headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json;charset=ms949",
             Authorization: "key=AAAAMFkjNys:APA91bGdpabEB2sSOHZu8yLpt1gnIWeeqKPtTrjCVgO-f5Kv2CDu9-olA11IvSxqANaY6GWFNl68XmzIvg5tX4SzPb5PU2P6v05TIuvJGPkuOP0gZzQvlXIZU8YxKB8_dToy_JOFrB9P"
           }
         }
